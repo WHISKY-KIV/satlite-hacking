5f7ff305e7d3a1983716dd76b99c135f9f9bb259
WHISKY-KIV <72498868+WHISKY-KIV@users.noreply.github.com>
2020-10-15 10:47:49 +0530

    Add files via upload

---
+++ sata.sh
@@ -0,0 +1,46 @@
+figlet 'Hack satellite'
+
+echo 'coded by pro hackers'
+
+sleep 2
+
+read -p '[=] Do you want to scan no of satellite: yes/no:-'
+
+sleep 2
+
+echo 'scanning no of satellites............'
+
+sleep 3
+
+
+echo 'satellite found'
+
+sleep 3
+
+echo '[=] Nasa'
+
+sleep 2
+
+read -p '[=]You pro hackers Do you want to hack Nasa :Yes/No:-'
+
+sleep 2
+
+echo 'you are hacking nasa'
+
+sleep 2
+
+
+echo 'Nasa hacked Details found'
+
+sleep 3
+
+read -p '[=] Do you want to see the hacked details :yes/no:-'
+
+sleep 3
+
+echo 'Poyi oombe myre nengal okka pro hackers namal pavam'
+
+sleep 2
+
+echo 'coded by markiv'
+
